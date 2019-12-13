37628c7a6200ffccd49f728a1ef10233e52d93a0
chianobi <chianobi@github.com>
2019-12-13 00:17:58 -0500

    added more partitioned fanfiction batches

--- scrapers/scraperfanfics.py
+++ scrapers/scraperfanfics.py
@@ -20,33 +20,31 @@ for line in test_pages:
 test_pages.close()
 print(urls)
 all = []
-for i, url in enumerate(urls):
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-    response = requests.post(url, headers = headers)
-#print( requests.options(test_page) )
+rootdir = './fictxt'
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        if file != '.DS_Store':
+            lines = open(os.path.join(subdir, file), 'r').readlines()
+            fic = {}
+            fic1 = {}
+            fic2 = {}
+            fic['text'] = ' '.join(lines[:30])
+            fic1['text'] = ' '.join(lines[30:60])
+            fic2['text'] = ' '.join(lines[60:90])
+            all.extend([fic, fic1, fic2])
+print(all)
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, "html.parser")
-        lines = soup.extract().text
-        linelst = lines.split('\n')
-        lines = [string.replace(u'\xa0', u' ') for string in linelst]
-        begind = index_containing_substring(lines, 'Any kind of feedback is greatly appreciated.')
-        if begind == -1:
-            begind = index_containing_substring(lines, 'Category:')
-        endind = lines.index('Comments')
-        file = open('fictxt/'+str(i)+'.txt', 'w+')
-        file.write('\n'.join(lines[begind+1:endind-1]))
-        file.close()
+with open('output1.jsonl', 'w', encoding='utf-8') as outfile:
+    for entry in all[:30]:
+        json.dump(entry, outfile)
+        outfile.write('\n')
 
-        fic = {}
-        fic['text'] = ' '.join(lines[begind+1:begind+51])
-        print(fic)
-        all.append(fic)
-    else:
-        pass
-        print(response.text)
+with open('output2.jsonl', 'w', encoding='utf-8') as outfile:
+    for entry in all[30:60]:
+        json.dump(entry, outfile)
+        outfile.write('\n')
 
-with open('output.jsonl', 'w') as outfile:
-    for entry in all:
+with open('output3.jsonl', 'w', encoding='utf-8') as outfile:
+    for entry in all[60:90]:
         json.dump(entry, outfile)
         outfile.write('\n')
